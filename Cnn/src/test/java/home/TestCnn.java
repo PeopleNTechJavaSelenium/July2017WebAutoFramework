a1c926b0ca8df33d2b946a352e4538a9df43984a
fhaque <haque26252@gmail.com>
2017-09-16 16:15:43 -0400

    maven runner

--- Cnn/src/test/java/home/TestCnn.java
+++ Cnn/src/test/java/home/TestCnn.java
@@ -1,6 +1,12 @@
 package home;
 
 import base.CommonAPI;
+import org.testng.annotations.Test;
 
 public class TestCnn extends CommonAPI {
+
+    @Test
+    public void homePage(){
+        getCurrentPageUrl();
+    }
 }
